05141ffafc79535f9c349956f4eef7ca97d69f50
zhoufei <15003707121@163.com>
2017-06-12 15:40:09 +0800

    项目内容优化

--- src/app/agri-tourisme/agri-tourisme.component.ts
+++ src/app/agri-tourisme/agri-tourisme.component.ts
@@ -12,4 +12,22 @@ export class AgriTourismeComponent implements OnInit {
   ngOnInit() {
   }
 
+  toLeft() {
+    var lis:any = document.getElementById("box").getElementsByTagName('li');
+    console.info(lis);
+    var box:any = document.getElementById("box");
+    box.style.width = lis.length * 240 + "px";
+    box.style.left = -240 + parseInt(box.style.left) + "px";
+
+    if (parseInt(box.style.left) <= -(lis.length - 3) * 240) {
+      box.style.left = -(lis.length - 3) * 240 + "px";
+    }
+  }
+  toRight() {
+    var box:any = document.getElementById("box");
+    box.style.left = 240 + parseInt(box.style.left) + "px";
+    if( box.style.left >= '0' ) {
+      box.style.left = '0';
+    }
+  }
 }
